e955a5fa2d2b031129467ae8b7665981b7cb3584
Dashinee PARMANUM <dparm023@uottawa.ca>
2025-07-30 10:45:25 -0400

    Update Python_Final+Ans.py

--- Python_Data_Analysis/Python_Scripts/Python_Final+Ans.py
+++ Python_Data_Analysis/Python_Scripts/Python_Final+Ans.py
@@ -24,6 +24,7 @@ import numpy as np
 data = pd.read_csv(r"C:\Users\dmpar\Documents\GitHub\statistics-projects-summer-2025\Python_Data_Analysis\Python_Datasets\oscar_age_female.csv")
 
 ## Display the first few rows.
+# head + print function to display first rows
 print(data.head())
 
 ## What are the column names? Are they formatted properly (no spaces, lowercase, etc.)?
